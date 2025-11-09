2b4ac08249cbdda70c4242475d74f6f97cc91e4d
luismeyer95 <luismeyer95@gmail.com>
2025-11-09 10:49:28 +0100

    add test for aliased import

---
+++ examples/rule-1-must-be-in-context/pass-aliased-import.ts
@@ -0,0 +1,18 @@
+import { balar as b } from "balar";
+
+const wrap = b.wrap.fns({
+  fetch: async (url: string[]) => {
+    return new Map(url.map((u) => [u, u + " data"]));
+  },
+});
+
+async function example() {
+  const urls = ["https://google.com", "https://github.com"];
+
+  const results = await b.run(urls, async (url) => {
+    const data = await wrap.fetch(url);
+    return data;
+  });
+
+  return results;
+}
